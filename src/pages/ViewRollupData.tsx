396affd69dd1d1957b2cec619d2c075976340f49
GreatSoshiant <salehi.mehdi70@gmail.com>
2023-05-28 15:16:29 -0700

    add option to download config.json file

--- src/pages/ViewRollupData.tsx
+++ src/pages/ViewRollupData.tsx
@@ -2,9 +2,11 @@
 import { useState, useEffect } from "react";
 import styles from "../styles/ViewRollupData.module.css";
 import { RollupConfigData } from './rollup';
+import Image from "next/image";
 
 const ViewRollupData = () => {
   const [data, setData] = useState<RollupConfigData | null>(null);
+  const [showData, setShowData] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -16,9 +18,29 @@ const ViewRollupData = () => {
     }
   }, []);
 
+  const downloadJSON = () => {
+    const element = document.createElement('a');
+    const file = new Blob([JSON.stringify(data, null, 2)], {type: 'application/json'});
+    element.href = URL.createObjectURL(file);
+    element.download = 'config.json';
+    document.body.appendChild(element);
+    element.click();
+  }
+
+  const toggleShowData = () => {
+    setShowData(!showData);
+  }
+
   if (!data) {
     return (
       <div className={styles.container}>
+              <Image
+          className={styles.logo} 
+          src="/logo.svg"
+          alt="Logo"
+          width={250}
+          height={250}
+          />
         <h1 className={styles.title}>No rollup data found.</h1>
       </div>
     );
@@ -26,10 +48,21 @@ const ViewRollupData = () => {
 
   return (
     <div className={styles.container}>
+            <Image
+          className={styles.logo} 
+          src="/logo.svg"
+          alt="Logo"
+          width={250}
+          height={250}
+          />
       <h1 className={styles.title}>Rollup Data:</h1>
-      <pre className={styles.data}>
-        {JSON.stringify(data, null, 2)}
-      </pre>
+      <button className={styles.button} onClick={downloadJSON}>Download JSON</button>
+      <button className={styles.button} onClick={toggleShowData}>Show More</button>
+      {showData && 
+        <pre className={styles.data}>
+          {JSON.stringify(data, null, 2)}
+        </pre>
+      }
     </div>
   );
 };
