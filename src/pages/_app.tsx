d859cea6917a22ea11dba06863cab703cd50537f
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-06-06 16:22:54 +0200

    change font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Space_Grotesk } from 'next/font/google';
 
 import 'primereact/resources/themes/lara-light-indigo/theme.css'; // theme
 import 'primereact/resources/primereact.css'; // core css
 import 'primeicons/primeicons.css'; // icons
 import 'primeflex/primeflex.css';
 
+import '@/styles/globals.css';
+
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main style={spaceGrotesk.style}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
