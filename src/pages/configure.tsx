99394efd153fde60b78f491b72b5710f2454b87e
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-06-07 14:05:27 +0200

    make nice

--- src/pages/configure.tsx
+++ src/pages/configure.tsx
@@ -7,7 +7,7 @@ import ViewRollupData from './ViewRollupData';
 import { RollupConfig, RollupConfigInput } from '@/components/RollupConfigInput';
 import { RollupContractsSummary } from '@/components/RollupContractsSummary';
 import { SetValidators } from '@/components/SetValidators';
-import SetBatchPoster from '@/components/SetBatchPoster';
+import { SetBatchPoster } from '@/components/SetBatchPoster';
 
 import { spaceGrotesk } from '@/fonts';
 import { deployRollup } from '@/utils/deployRollup';
@@ -147,7 +147,7 @@ export default function Configure() {
           <>
             <StepTitle>Configure Batch Poster</StepTitle>
             <div className="h-4" />
-            <SetBatchPoster onDone={() => setStep(Step.Review)} />
+            <SetBatchPoster onNext={() => setStep(Step.Review)} />
           </>
         )}
 
