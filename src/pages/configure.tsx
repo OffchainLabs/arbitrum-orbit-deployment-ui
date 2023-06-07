d9b041958bf9d6ab6e2ef8266a0155a05cecfd1b
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-06-07 14:09:29 +0200

    rename file

--- src/pages/configure.tsx
+++ src/pages/configure.tsx
@@ -6,7 +6,7 @@ import { RollupConfig, RollupConfigInput } from '@/components/RollupConfigInput'
 import { RollupContractsSummary } from '@/components/RollupContractsSummary';
 import { SetValidators } from '@/components/SetValidators';
 import { SetBatchPoster } from '@/components/SetBatchPoster';
-import ViewRollupData from '@/components/ViewRollupData';
+import ViewRollupData from '@/components/Review';
 
 import { spaceGrotesk } from '@/fonts';
 import { deployRollup } from '@/utils/deployRollup';
