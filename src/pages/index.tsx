95f138c982ea1988dbf23c29fe7490cd313609ec
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-06-08 14:45:55 +0200

    feat: update route from /configure to /deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
         <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
-            href="/configure"
+            href="/deployment"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
