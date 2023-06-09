3e4facbf34cf3f6458a87b69f16ad37ba644fdea
spsjvc <dragisaspasojevic@gmail.com>
2023-06-09 16:18:27 +0200

    feat: keep state in query params (#19)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app';
+import { QueryParamProvider } from 'use-query-params';
+import { NextAdapter } from 'next-query-params';
+import queryString from 'query-string';
 
 import 'primereact/resources/themes/lara-light-indigo/theme.css'; // theme
 import 'primereact/resources/primereact.css'; // core css
@@ -11,7 +14,16 @@ import { spaceGrotesk } from '@/fonts';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main style={spaceGrotesk.style}>
-      <Component {...pageProps} />
+      <QueryParamProvider
+        adapter={NextAdapter}
+        options={{
+          searchStringToObject: queryString.parse,
+          objectToSearchString: queryString.stringify,
+          updateType: 'replaceIn',
+        }}
+      >
+        <Component {...pageProps} />
+      </QueryParamProvider>
     </main>
   );
 }
