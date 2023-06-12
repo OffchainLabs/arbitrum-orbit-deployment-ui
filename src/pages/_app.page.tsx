b9c52faf9ecbc84fb37aa7477224400cb4f84d64
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-06-12 19:37:34 +0200

    feat: add posthog

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,6 +5,8 @@ import queryString from 'query-string';
 import { WagmiConfig } from 'wagmi';
 import { arbitrumGoerli } from 'wagmi/chains';
 import { RainbowKitProvider, ConnectButton, lightTheme } from '@rainbow-me/rainbowkit';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 import '@rainbow-me/rainbowkit/styles.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css'; // theme
@@ -16,49 +18,69 @@ import '@/styles/globals.css';
 import { wagmiClient, chains, appInfo } from '@/setupWagmi';
 import { spaceGrotesk } from '@/fonts';
 
+if (typeof window !== 'undefined' && typeof process.env.NEXT_PUBLIC_POSTHOG_KEY === 'string') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV !== 'production') {
+        // when in dev, you can see data that would be sent in prod (in devtools)
+        posthog.debug();
+      }
+    },
+    // store data in temporary memory that expires with each session
+    persistence: 'memory',
+    // by default posthog autocaptures (sends) events such as onClick, etc
+    // we set up our own events instead
+    autocapture: false,
+    disable_session_recording: true,
+  });
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryParamProvider
-      adapter={NextAdapter}
-      options={{
-        searchStringToObject: queryString.parse,
-        objectToSearchString: queryString.stringify,
-        updateType: 'replaceIn',
-      }}
-    >
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider
-          chains={chains}
-          appInfo={appInfo}
-          initialChain={arbitrumGoerli}
-          theme={{
-            ...lightTheme({
-              accentColor: '#243145',
-            }),
-            fonts: {
-              body: spaceGrotesk.style.fontFamily,
-            },
-          }}
-        >
-          <div className="items flex w-full flex-col" style={spaceGrotesk.style}>
-            <header className="flex w-full justify-center">
-              <div className="flex w-[1024px] flex-col gap-2 py-4">
-                <div className="flex w-full items-center justify-between">
-                  <h1 className="text-2xl font-bold">Arbitrum Orbit</h1>
-                  <ConnectButton />
+    <PostHogProvider client={posthog}>
+      <QueryParamProvider
+        adapter={NextAdapter}
+        options={{
+          searchStringToObject: queryString.parse,
+          objectToSearchString: queryString.stringify,
+          updateType: 'replaceIn',
+        }}
+      >
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider
+            chains={chains}
+            appInfo={appInfo}
+            initialChain={arbitrumGoerli}
+            theme={{
+              ...lightTheme({
+                accentColor: '#243145',
+              }),
+              fonts: {
+                body: spaceGrotesk.style.fontFamily,
+              },
+            }}
+          >
+            <div className="items flex w-full flex-col" style={spaceGrotesk.style}>
+              <header className="flex w-full justify-center">
+                <div className="flex w-[1024px] flex-col gap-2 py-4">
+                  <div className="flex w-full items-center justify-between">
+                    <h1 className="text-2xl font-bold">Arbitrum Orbit</h1>
+                    <ConnectButton />
+                  </div>
+                  <div>
+                    <span className="rounded-lg bg-[#FFEED3] px-3 py-2 text-sm text-[#60461F]">
+                      This tool is in alpha, and for local devnet deployment only.
+                    </span>
+                  </div>
                 </div>
-                <div>
-                  <span className="rounded-lg bg-[#FFEED3] px-3 py-2 text-sm text-[#60461F]">
-                    This tool is in alpha, and for local devnet deployment only.
-                  </span>
-                </div>
-              </div>
-            </header>
+              </header>
 
-            <Component {...pageProps} />
-          </div>
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </QueryParamProvider>
+              <Component {...pageProps} />
+            </div>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </QueryParamProvider>
+    </PostHogProvider>
   );
 }
