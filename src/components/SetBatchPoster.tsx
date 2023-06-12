5de2491f3f1ac19dd4822cefbaa62818780370a8
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-06-12 18:53:50 +0200

    fix: update loading text

--- src/components/SetBatchPoster.tsx
+++ src/components/SetBatchPoster.tsx
@@ -149,7 +149,7 @@ export function SetBatchPoster({ onNext }: { onNext: () => void }) {
             disabled={status === 'loading'}
             className="w-full rounded-lg bg-[#243145] px-3 py-2 text-2xl text-white"
           >
-            {status === 'loading' ? 'Loading...' : 'Submit'}
+            {status === 'loading' ? 'Waiting...' : 'Submit'}
           </button>
         )}
       </form>
