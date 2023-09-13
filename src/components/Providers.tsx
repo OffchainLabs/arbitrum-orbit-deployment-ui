6cc0d810fd116fb85b1fc4afd2ea98a3af4ab5f0
Doug Lance <doug.lance@gmail.com>
2023-09-13 07:00:41 -0400

    feat: adds twitter title and description metadata (#61)
    * feat: adds twitter title and description metadata
    
    * chore: updates root layout to server component
    
    * updates meta description

---
+++ src/components/Providers.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { spaceGrotesk } from '@/fonts';
+import { RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+import { WagmiConfig } from 'wagmi';
+import { arbitrumGoerli } from 'wagmi/chains';
+import { appInfo, chains, wagmiConfig } from '@/setupWagmi';
+
+if (typeof window !== 'undefined' && typeof process.env.NEXT_PUBLIC_POSTHOG_KEY === 'string') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV !== 'production') {
+        // when in dev, you can see data that would be sent in prod (in devtools)
+        posthog.debug();
+      }
+    },
+    // store data in temporary memory that expires with each session
+    persistence: 'memory',
+    autocapture: true,
+    disable_session_recording: true,
+  });
+}
+
+export const Providers = ({ children }: any) => {
+  return (
+    <PostHogProvider client={posthog}>
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider
+          chains={chains}
+          appInfo={appInfo}
+          initialChain={arbitrumGoerli}
+          theme={{
+            ...lightTheme({
+              accentColor: '#243145',
+            }),
+            fonts: {
+              body: spaceGrotesk.style.fontFamily,
+            },
+          }}
+        >
+          {children}{' '}
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </PostHogProvider>
+  );
+};
