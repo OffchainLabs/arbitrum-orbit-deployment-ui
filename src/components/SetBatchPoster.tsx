d56e57907a56c93b41cf0d15c1e03c9c595e9054
Doug Lance <doug.lance@gmail.com>
2023-09-19 14:19:01 -0400

    Adds hexstring assertion for privatekey

--- src/components/SetBatchPoster.tsx
+++ src/components/SetBatchPoster.tsx
@@ -9,6 +9,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Wallet } from '@/types/RollupContracts';
 import { AddressSchema, PrivateKeySchema } from '@/utils/schemas';
+import { assertIsHexString } from '@/utils/validators';
 
 const batchPosterSchema = z.object({
   batchPosterAddress: AddressSchema,
@@ -34,6 +35,8 @@ export const SetBatchPoster = () => {
   });
 
   const onSubmit = (data: BatchPosterFormValues) => {
+    assertIsHexString(data.batchPosterPrivateKey);
+
     const payload = {
       address: data.batchPosterAddress,
       privateKey: data.batchPosterPrivateKey,
