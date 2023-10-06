7a1b634c91c9dabd282b553129b7701add09e0c0
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-10-06 11:41:29 +0200

    update error message

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -2,6 +2,6 @@ import { isAddress } from 'viem';
 
 export function assertIsAddress(value: any): asserts value is `0x${string}` {
   if (typeof value !== 'string' || !isAddress(value)) {
-    throw new Error('Value must be of type `0x${string}`');
+    throw new Error(`${value} is not a valid address`);
   }
 }
