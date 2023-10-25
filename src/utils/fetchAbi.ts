0780beae8f3093cb9dbcdcfa6b6364e5d1189a63
Doug Lance <doug.lance@gmail.com>
2023-10-25 11:18:38 -0400

    feat: change sepolia block explorer to arbiscan

--- src/utils/fetchAbi.ts
+++ src/utils/fetchAbi.ts
@@ -2,21 +2,16 @@ import fetch from 'node-fetch';
 
 import { ChainId } from '@/types/ChainId';
 
-type BlockExplorerProvider = 'arbiscan' | 'blockscout';
-
 type BlockExplorerApi = {
-  provider: BlockExplorerProvider;
   url: string;
 };
 
 const blockExplorerApis: Record<ChainId, BlockExplorerApi> = {
   [ChainId.ArbitrumGoerli]: {
-    provider: 'arbiscan',
     url: 'https://api-goerli.arbiscan.io/api',
   },
   [ChainId.ArbitrumSepolia]: {
-    provider: 'blockscout',
-    url: 'https://sepolia-explorer.arbitrum.io/api',
+    url: 'https://api-sepolia.arbiscan.io/api',
   },
 };
 
@@ -26,22 +21,7 @@ export function getRequestUrl(chainId: ChainId, address: `0x${string}`) {
 
 export async function fetchAbi(chainId: ChainId, address: `0x${string}`) {
   const requestUrl = getRequestUrl(chainId, address);
-
-  return blockExplorerApis[chainId].provider === 'arbiscan'
-    ? fetchAbiFromArbiscan(requestUrl)
-    : fetchAbiFromBlockscout(requestUrl);
-}
-
-export async function fetchAbiFromArbiscan(url: string) {
-  const response = await fetch(url);
+  const response = await fetch(requestUrl);
   const responseJson = await response.json();
-
   return responseJson;
-}
-
-export async function fetchAbiFromBlockscout(url: string) {
-  const response = await fetch(url);
-  const responseJson = await response.json();
-
-  return JSON.parse((responseJson as any).result);
 }
