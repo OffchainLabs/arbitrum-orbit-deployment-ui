94c7b42f8169e2c90ac3a313766b57a60af81228
Doug <4741454+douglance@users.noreply.github.com>
2024-02-07 08:59:25 -0500

    hotfix: restores download button

--- app/deployment/step/download/page.tsx
+++ app/deployment/step/download/page.tsx
@@ -1,18 +1,30 @@
 'use client';
 
 import { CodeComponent } from '@/components/CodeComponent';
+import { useDeploymentPageContext } from '@/components/DeploymentPageContext';
 import { DeploymentSummary } from '@/components/DeploymentSummary';
 import { StepTitle } from '@/components/StepTitle';
 import { useConfigDownloads } from '@/hooks/useConfigDownloads';
 
 export default function DownloadPage() {
-  const { rollupConfigDownloadData, rollupConfigDisplayData, l3Config } = useConfigDownloads();
+  const { rollupConfigDownloadData, rollupConfigDisplayData, l3Config, downloadZippedConfigs } =
+    useConfigDownloads();
+  const [{}, dispatch] = useDeploymentPageContext();
 
   return (
     <div className="mx-0 my-2 grid grid-cols-2 gap-4">
       <div>
         <StepTitle>Download Config</StepTitle>
         <p className="my-1">Configuration files are required to deploy locally.</p>
+        <button
+          onClick={() => {
+            downloadZippedConfigs();
+            dispatch({ type: 'set_is_download_completed', payload: true });
+          }}
+          className="w-fit rounded-md border border-gray-300 bg-white px-5 py-2  text-lg text-black hover:bg-gray-50"
+        >
+          <i className="pi pi-download mr-2"></i> &nbsp; Download zip files
+        </button>
         <div className="mx-0 my-2">
           <div>
             <h4 className="font-bold">Rollup Config</h4>
